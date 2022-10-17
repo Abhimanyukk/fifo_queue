97c88b116d85bf032621c4a0fcbf489d2a223fd4
= <=>
2022-10-17 10:48:52 +0530

    Add headers

---
+++ examples/ring_queue_ex.c
@@ -0,0 +1,10 @@
+/**
+ * @file ring_queue_ex.c
+ * @author Abhimanyu KK
+ * @brief Ring queue example
+ * @version 0.1
+ * @date 2022-10-17
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
---
+++ simple_queue/inc/ring_queue.h
@@ -0,0 +1,10 @@
+/**
+ * @file ring_queue.h
+ * @author Abhimanyu KK
+ * @brief Ring queue header file
+ * @version 0.1
+ * @date 2022-10-17
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
